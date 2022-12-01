7382d6fbace79cf4d0daa3c880edb907c850a9e9
MartinoPapa <martino.papa.tn@gmail.com>
2022-12-01 13:39:30 +0100

    spero funzionino zio pera

--- query1.sql
+++ query1.sql
@@ -1,8 +1,8 @@
-select movies.director, COALESCE(min(gross-budget),-1) AS min, COALESCE(max(gross-budget),-1) AS max , COALESCE(TRUNC(avg((gross-budget)::decimal),2),-1) AS avg 
+select oldDirector.director, COALESCE(min(gross-budget),-1) AS min, COALESCE(max(gross-budget),-1) AS max , COALESCE(TRUNC(avg((gross-budget)::decimal),2),-1) AS avg 
 from (select director 
 from directors 
 where (date_part('year', CURRENT_DATE)-yearofbirth > 50)) as oldDirector left join movies
 on oldDirector.director = movies.director
-group by movies.director
-order by movies.director, min, max, avg;
+group by oldDirector.director
+order by oldDirector.director, min, max, avg;
 
